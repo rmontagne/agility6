3e36e58f45dd995a9527e20d9a3f5786060f4032
Romain Montagne <rmontagne@supvize.com>
2018-05-02 11:50:52 +0200

    Update User class

--- classes/User.php
+++ classes/User.php
@@ -11,5 +11,10 @@ class User extends ORM {
     public function getFirstname() {
         return $this->firstname;
     }
+    
+    public function setFirstname($firstname) {
+        $this->firstname = $firstname;
+        return $this;
+    }
        
 }
