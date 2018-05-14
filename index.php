ea4693004753e9b9dd755ceebb7fcff826ed9ee6
aurelieGarnier <aurelie.garnier86@gmail.com>
2018-05-14 20:00:03 +0200

    menageAurelie

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-    require_once(dirname(__FILE__).'/config.inc.php');
-    require_once(dirname(__FILE__).'/classes/Router.php');
-    require_once(dirname(__FILE__).'/classes/Template.php');
-    
-    Router::run();
