ff9674c091f839bfaab18fc5ef79d188def23cef
aurelieGarnier <aurelie.garnier86@gmail.com>
2018-05-14 19:37:53 +0200

    Ajout de la fonction arrayToTable sans template pour le tableau

---
+++ Template.php
@@ -0,0 +1,266 @@
+<?php
+
+require_once(dirname(__FILE__).'/User.php');
+require_once(dirname(__FILE__).'/ORM.php');
+    
+    class Template {
+        
+        /*
+            - Condition (Baptiste)
+            - Boucle
+        */
+        
+        public static function render($tplName, $params) {
+            
+            if (!file_exists(dirname(__FILE__).'/../templates/'.$tplName.'.isml')) {
+                return 'Template '.$tplName.' not found !';
+            }
+            
+            $content = file_get_contents(dirname(__FILE__).'/../templates/'.$tplName.'.isml');
+        
+
+            return self::parseContent($content, $params);
+        }
+        
+        
+        static private function parseContent($content, $params) {
+            
+            //COMMENTAIRE
+            $content = self::comment($content);
+            
+            //ISINCLUDE
+            $content = self::includeTpl($content, $params);
+            
+            //LOOP
+            $content = self::loop($content, $params);
+            
+            //VARIABLE SIMPLE
+            $content = self::parseSimpleVar($content, $params);
+            
+            //VARIABLE OBJECT
+            $content = self::parseObjectVar($content, $params);
+            
+            //CONDITION
+            $content = self::condition($content, $params);
+                         
+            //BOUCLE
+            $content = self::decorate($content, $params);
+            
+            //TABLE
+            $content = self::arrayToTable($content, $params);
+            
+            return $content;
+        }
+        
+        static private function comment($content) {
+            //Allow only one decorator
+            if (preg_match_all('#(?s)<iscomment(.(?:(?!<iscomment).)*?)</iscomment>#', $content, $matches)) {
+                $content = str_replace($matches[0],'',$content);
+            }
+            return $content;
+        }
+        
+        static private function decorate($content, $params) {       
+            //Allow only one decorator
+            if (preg_match('#<isdecorate template=\'([a-z_]+)\'>(.*)?</isdecorate>#s', $content, $matches)) {
+                
+                $decorator = self::render($matches[1], $params);
+                $isreplace = self::parseContent($matches[2], $params);
+                
+                return str_replace('<isreplace />', $isreplace, $decorator);               
+            }
+            
+            return $content;
+        }
+        
+        static private function loop($content, $params) {
+            if (preg_match_all('#<isloop\s+items\s*=\s*"\s*\$\{\s*(\w+)\s*\}\s*"\s+alias\s*=\s*"(\w+)".*>(.*)<\/isloop>#sU', $content, $matches, PREG_SET_ORDER)) {
+                
+                foreach($matches as $match) {
+                    if (isset($params[$match[1]]) && is_array($params[$match[1]])){
+                        $inFor='';
+                        foreach($params[$match[1]] as $item) {
+                            $paramsLoop = array($match[2] => $item);
+                            $params2 = array_merge($params, $paramsLoop);
+                            
+                            $out = self::condition($match[3], $params2);
+                            if (is_object($item)) {
+                                $inFor .= self::parseObjectVar($out, $params2);
+                            } else {
+                                $inFor .= self::parseSimpleVar($out, $params2);
+                            }
+                        }
+                        
+                        $outFor = self::parseContent($inFor, $params);
+                        $content = str_replace($match[0],$outFor, $content);
+                    } else {
+                        die('ERREUR DE PARAMETRE, TABLEAU D OBJET ATTENDU');
+                    }
+                }
+            }
+            return $content;
+        }
+        
+        static private function condition($content, $params) {
+            $recursivity = 'off';
+            if (preg_match('#(?s)<isif(.(?:(?!<isif).)*?)</isif>#', $content, $matches)) {
+                $total = $matches[0];
+                $recursivity = 'on';
+                preg_match('#(?s).*condition="\${(.*?)}".*?>(.*)#', $matches[1], $matches);
+                $condition = $matches[1];
+                foreach ($params as $key => $param) {
+                    $condition = str_replace($key, '$'.$key, $condition);
+                }
+                $variable = '';
+                foreach ($params as $key => $value) {
+                    if (is_object($value) || is_array($value)) {
+                        $variable .= '$'.$key.' = unserialize(\''.serialize($value).'\');';
+                    } else {
+                        $variable .= '$'.$key.' = "'.$value.'";';
+                    }
+                }
+                $condition = 'return ('.$condition.');';
+                $return = eval($variable.$condition);
+                if (!$return) {
+                    $content = str_replace($total,'',$content);
+                }
+                if ($return) {
+                    $content = str_replace($total, $matches[2], $content);
+                }
+            }
+            if($recursivity == 'on') {
+                return self::condition($content, $params);
+            }
+            return $content;
+        }
+        
+        static private function includeTpl($content, $params) {
+            
+            if (preg_match_all('#<isinclude.*template.*="(.+)".*/>#', $content, $matches)) { 
+                foreach ($matches[1] as $match) { 
+                    $tplRequire = $match; 
+                    $requireContent = self::render($tplRequire,$params); 
+                    $content = preg_replace('#<isinclude.*template.*="'.$tplRequire.'".*/>#',$requireContent, $content); 
+                } 
+            }
+            
+            if (preg_match_all('#<isinclude.*remote.*="(.+)".*/>#', $content, $matches)) { 
+                foreach ($matches[1] as $match) {
+                    
+                    $tmp            = explode('-', $match);
+                    $controller     = $tmp[0];
+                    $action         = $tmp[1];
+
+                    $requireContent = Router::callController($controller, $action); 
+                    $content = preg_replace('#<isinclude.*remote.*="'.$match.'".*/>#',$requireContent, $content); 
+                } 
+            }
+            
+            return $content;
+        }
+        
+        static private function arrayToTable($content, $params) {
+            if (preg_match_all('#<istable \${(\w+)} *\/>#U', $content, $matches)) {
+                $property   = [];
+                
+                foreach ($matches[1] as $match) {
+                    $tmp    = $match;
+                    $table  = '<table id="tbl_'.$tmp.'" border="1" class="table table-bordered table-striped table-condensed sortable"><thead><tr>';
+                    if (isset($params[$match])) {
+                        $array      = $params[$match];
+                        
+                        // amélioration : table dans un template
+                        
+                        // We find the class and its attibutes
+                        $myClass    = ucfirst(substr($match, 0, -1));
+                        $myInstance = new $myClass();
+                        $attributes = $myInstance->getAttributes();
+                        
+                        // We create the array containing the mysql table
+                        foreach ($array as $values) {
+                            foreach ($attributes as $attribute => $value) {
+                                $method = 'get'.ucfirst($attribute);
+                                
+                                if (method_exists($myClass, $method)) {
+                                    if ($attribute == 'table') {
+                                        continue;
+                                    }
+                                    $property[$attribute] = $values->$method();
+                                }
+                            }
+                            $properties[] = $property;
+                        }
+                        
+                        // We build the html table
+                        foreach ($properties[0] as $key => $col) {
+                            $table .= '<th>'.$key.'</th>';
+                        }
+                        $table .= '<th></th></tr></thead><tbody>';
+                        
+                        foreach ($properties as $row) {
+                            $table .= '<tr>';
+                            foreach ($row as $key => $col){
+                                if ($key == 'id') {
+                                    $currentId = $row[$key];
+                                }
+                                $table .= '<td> '.$col.' </td>';
+                            }
+                            $table .= '<td style="width: 200px;"><input type="button" class="btn-edit"  onclick="location.href=\''.ucfirst($tmp).'-Update-'.$currentId.'\';" value="Editer" />
+                                    <input type="button" class="btn-remove" onclick="location.href=\''.ucfirst($tmp).'-Delete-'.$currentId.'\';" value="Supprimer" /></td></tr>';
+                        }
+                        $table .= '</tbody></table>';
+                        $content = str_replace('<istable ${'.$tmp.'} />', $table, $content);
+                    }
+                }
+            }
+            return $content;
+        }
+        
+        static private function parseSimpleVar($content, $params) {
+            if (preg_match_all('#\${([a-zA-Z]+)}#', $content, $matches)) {
+                foreach ($matches[1] as $match) {
+                    if (isset($params[$match]) && !is_array($params[$match])) {
+                        $content = str_replace('${'.$match.'}', $params[$match], $content);
+                    }
+                }            
+            }
+            
+            return $content;
+        }
+        
+        static private function parseObjectVar($content, $params) {
+            if (preg_match_all('#\${([a-zA-Z.]+)}#', $content, $matches)) {
+                
+                foreach ($matches[1] as $match) {
+                    
+                    $attributes = explode('.', $match);
+                    $mainObject = $params[$attributes[0]];
+                    
+                    if (!is_array($params[$attributes[0]])) {
+                       
+                        for($i = 1; $i < count($attributes); $i++) {
+                            
+                            $getter = 'get'.ucfirst($attributes[$i]);
+                            if (method_exists($mainObject, $getter)) {
+                                $mainObject = $mainObject->{$getter}();
+                            } else if (property_exists($mainObject, $attributes[$i])) {
+                                $mainObject = $mainObject->{$attributes[$i]};
+                            } else {
+                                $mainObject = '';
+                                break;
+                            }
+                        }
+                        if (!is_object($mainObject)) {
+                            $content = str_replace('${'.$match.'}', $mainObject, $content);
+                        } else {
+                            $content = str_replace('${'.$match.'}', '[Object]', $content);
+                        }
+                    }
+                }            
+            }
+            
+            return $content;
+        }
+        
+        
+    }
